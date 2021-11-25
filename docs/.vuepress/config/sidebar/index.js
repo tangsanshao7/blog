104a14877ac8b3fcbb79ececd5f197363e803edc
tangsanshao7 <393714523@qq.com>
2021-11-25 16:58:20 +0800

    新添笔记

--- docs/.vuepress/config/sidebar/index.js
+++ docs/.vuepress/config/sidebar/index.js
@@ -5,7 +5,7 @@ module.exports = {
     {
       title: "前端 Front-end",
       collapsable: true,
-      children: ["frontend/vue", "frontend/git", "frontend/jacascript"],
+      children: ["frontend/vue", "frontend/git", "frontend/javascript"],
     },
 
     {
